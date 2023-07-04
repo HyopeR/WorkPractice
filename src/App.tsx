a114a0e3fec66975a5a2a0b3fe057c962b8974c5
Tolgahan Çelik <tol_celik@hotmail.com>
2023-07-04 21:18:31 +0300

    First blood.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text style={{fontSize: 28, alignSelf: 'center'}}>Hello World</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
